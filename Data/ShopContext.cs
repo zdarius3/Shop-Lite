2e75b8375bbbba1c186db42d16d9a7646618895f
zdarius3 <damian.romalvarez@gmail.com>
2025-08-15 03:22:53 -0400

    feat(Data): Added ShopContext

---
+++ Data/ShopContext.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using ShopLite.Entities;
+
+namespace ShopLite.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<OrderItem> OrderItems { get; set; } = null!;
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //define the relation between OrderItem and Order
+            modelBuilder.Entity<OrderItem>()
+                .HasOne(oi => oi.Order)
+                .WithMany(o => o.OrderItems)
+                .HasForeignKey(oi => oi.OrderId)
+                .OnDelete(DeleteBehavior.Cascade);  //if deleted one Order, delete its OrderItems
+
+            //define the relation between OrderItem and Product 
+            modelBuilder.Entity<OrderItem>()
+                .HasOne(oi => oi.Product)
+                .WithMany(p => p.OrderItems)
+                .HasForeignKey(oi => oi.ProductId)
+                .OnDelete(DeleteBehavior.Restrict); //prevents deleting a Product if it still has OrderItems
+
+            //define the relation between Product and Category
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict); //prevents deleting a Category if it still has Products
+
+            //turn the Status enum into a string 
+            modelBuilder.Entity<Order>()
+                .Property(o => o.Status)
+                .HasConversion<string>();
+        }
+    }
+}
