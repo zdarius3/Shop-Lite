ce3b211353848f12c388b053eece19ec92dd5911
zdarius3 <damian.romalvarez@gmail.com>
2025-08-18 02:05:17 -0400

    fix: Fixed OrderItems DTOs not having a corresponding OrderID attribute

--- DTOs/CreateOrderItemDTO.cs
+++ DTOs/CreateOrderItemDTO.cs
@@ -8,6 +8,9 @@ namespace ShopLite.DTOs
 
         [Required]
         public int ProductId { get; init; }
+        
+        [Required]
+        public int OrderId { get; init; }
 
         [Required]
         [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1.")]
--- DTOs/OrderItemDTO.cs
+++ DTOs/OrderItemDTO.cs
@@ -8,6 +8,7 @@ namespace ShopLite.DTOs
         public int Id { get; init; }
 
         public int ProductId { get; init; }
+        public int OrderId { get; init; }
 
         public string ProductName { get; init; } = null!;
 
