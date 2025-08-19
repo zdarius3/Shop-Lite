74c3c042d49198ab4eccbf998487a705dc9800ab
zdarius3 <damian.romalvarez@gmail.com>
2025-08-19 02:21:18 -0400

    feat: Configured most dependencies and added Swagger package for testing

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using ShopLite.Data;
+using ShopLite.Interfaces;
 using ShopLite.Middlewares;
+using ShopLite.Repositories;
+using ShopLite.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,17 +14,35 @@ builder.Services.AddDbContext<ShopContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
         new MySqlServerVersion(new Version(8, 0, 31))));
 
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<IOrderItemRepository, OrderItemRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<IOrderItemService, OrderItemService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 app.UseMiddleware<GlobalErrorHandlerMiddleware>();
+app.MapControllers();
 
 app.Run();
 
