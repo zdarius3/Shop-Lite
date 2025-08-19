29f4948d7af1ad8fd6ecb04f472e5953645c182d
zdarius3 <damian.romalvarez@gmail.com>
2025-08-19 01:28:26 -0400

    fix(Category.cs): Fixed the Products list not being initialized directly

--- Entities/Category.cs
+++ Entities/Category.cs
@@ -14,6 +14,6 @@ namespace ShopLite.Entities
         [MaxLength(50)]
         public string Name { get; set; } = null!;
 
-        public ICollection<Product>? Products { get; set; }
+        public List<Product> Products { get; set; } = new List<Product>();
     }
 }
